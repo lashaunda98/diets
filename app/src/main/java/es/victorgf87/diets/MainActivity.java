298af6b2851393f2582e2c89090a36a8be21ae5c
Victor Gonzalez <victor.gf87@gmail.com>
2015-10-10 03:43:47 +0200

    Left a wolfram example on MainActivity

--- app/src/main/java/es/victorgf87/diets/MainActivity.java
+++ app/src/main/java/es/victorgf87/diets/MainActivity.java
@@ -2,12 +2,21 @@ package es.victorgf87.diets;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
 import com.dacer.androidcharts.LineView;
+import com.wolfram.alpha.WAEngine;
+import com.wolfram.alpha.WAException;
+import com.wolfram.alpha.WAPlainText;
+import com.wolfram.alpha.WAPod;
+import com.wolfram.alpha.WAQuery;
+import com.wolfram.alpha.WAQueryResult;
+import com.wolfram.alpha.WASubpod;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -107,8 +116,85 @@ public class MainActivity extends AppCompatActivity
         ButterKnife.bind(this);
 
         List<ExerciseActivity> activities= StorerFactory.create(this).getActivitiesList();
-        int a=3;
-        int b=a;
+
+        
+
+    }
+
+    private void wolfram()
+    {
+
+
+        HandlerThread thread=new HandlerThread("name");
+        thread.start();
+        Handler handler=new Handler(thread.getLooper());
+        handler.post(new Runnable(){
+
+            @Override
+            public void run() {
+                String wolfram_api_key=BuildConfig.WOLFRAM_API_KEY;
+                String input = "falafel";
+                // The WAEngine is a factory for creating WAQuery objects,
+                // and it also used to perform those queries. You can set properties of
+                // the WAEngine (such as the desired API output format types) that will
+                // be inherited by all WAQuery objects created from it. Most applications
+                // will only need to crete one WAEngine object, which is used throughout
+                // the life of the application.
+                WAEngine engine = new WAEngine();
+
+                // These properties will be set in all the WAQuery objects created from this WAEngine.
+                engine.setAppID(wolfram_api_key);
+                //engine.addFormat("plaintext");
+
+
+                // Create the query.
+                WAQuery query = engine.createQuery();
+
+                // Set properties of the query.
+                query.setInput(input);
+
+                try {
+                    // For educational purposes, print out the URL we are about to send:
+                    System.out.println("Query URL:");
+                    System.out.println(engine.toURL(query));
+                    System.out.println("");
+
+                    // This sends the URL to the Wolfram|Alpha server, gets the XML result
+                    // and parses it into an object hierarchy held by the WAQueryResult object.
+                    WAQueryResult queryResult = engine.performQuery(query);
+
+                    if (queryResult.isError()) {
+                        System.out.println("Query error");
+                        System.out.println("  error code: " + queryResult.getErrorCode());
+                        System.out.println("  error message: " + queryResult.getErrorMessage());
+                    } else if (!queryResult.isSuccess()) {
+                        System.out.println("Query was not understood; no results available.");
+                    } else {
+                        // Got a result.
+                        System.out.println("Successful query. Pods follow:\n");
+                        for (WAPod pod : queryResult.getPods()) {
+                            if (!pod.isError()) {
+                                System.out.println(pod.getTitle());
+                                System.out.println("------------");
+                                for (WASubpod subpod : pod.getSubpods()) {
+                                    for (Object element : subpod.getContents()) {
+                                        if (element instanceof WAPlainText) {
+                                            System.out.println(((WAPlainText) element).getText());
+                                            System.out.println("");
+                                        }
+                                    }
+                                }
+                                System.out.println("");
+                            }
+                        }
+                        // We ignored many other types of Wolfram|Alpha output, such as warnings, assumptions, etc.
+                        // These can be obtained by methods of WAQueryResult or objects deeper in the hierarchy.
+                    }
+                } catch (WAException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
